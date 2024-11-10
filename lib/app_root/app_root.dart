778558b97ad1bdcf52ae795a8e59a5234a29b4b5
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2024-11-10 14:07:30 +0200

    fill the app rout file

---
+++ lib/app_root/app_root.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class DeafGainApp extends StatelessWidget {
+  const DeafGainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
