8d56be9dd932c7b0772a45e7a9fc5da9a9aad080
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2024-11-10 14:46:08 +0200

    add packages for the app root and create home screen file

--- lib/app_root/app_root.dart
+++ lib/app_root/app_root.dart
@@ -1,4 +1,8 @@
+import 'package:deaf_gain/screens/home_screen.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 
 
@@ -8,8 +12,20 @@ class DeafGainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return DevicePreview(
+      enabled: false,
+      builder: (context) => SafeArea(
+        child: ScreenUtilInit(
+            designSize: const Size(393, 852),
+            minTextAdapt: true,
+            builder: (context, child) =>  GetMaterialApp(
+              locale: Locale('ar'),
+              debugShowCheckedModeBanner: false,
+              home: HomeScreen(),
 
+
+            )),
+      ),
     );
   }
 }
